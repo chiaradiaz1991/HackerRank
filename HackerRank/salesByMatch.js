416f5f2e08c071c5a545211c7909503e31922952
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-11-26 19:35:33 +0100

    first commit

---
+++ HackerRank/salesByMatch.js
@@ -0,0 +1,41 @@
+// Alex works at a clothing store. There is a large pile of socks that must be paired by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+// Function Description
+
+// Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+
+// sockMerchant has the following parameter(s):
+
+// n: the number of socks in the pile
+// ar: the colors of each sock
+// Input Format
+
+// The first line contains an integer , the number of socks represented in .
+// The second line contains  space-separated integers describing the colors  of the socks in the pile.
+
+// Output
+// 3
+
+let NumberOfSocks = 9;
+let color = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+let arr = [];
+
+function sockMerchant(n, ar) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    ar.sort();
+    for (let i = 0; i < n; i++) {
+      console.log(ar[i], ar[i + 1])
+      if (ar[i] === ar[i + 1]) {
+        i++;
+        count++;
+      }
+    }
+    return count;
+  }
+}
+
+console.log(sockMerchant(NumberOfSocks, color));
